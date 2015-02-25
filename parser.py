1a3631cdb68e06c52129d0e5bed6e18bfcef007e
parlex <premer@outlook.com>
2015-02-26 00:01:44 +0100

    Started on a script that takes out the tables with attributes from imdb because its here the schedule is located. next up is to extract that data out of the lists that now is being made.

---
+++ parser.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+from HTMLParser import HTMLParser
+import urllib2 as url
+
+response = url.urlopen("http://www.imdb.com/title/tt1358522/tvschedule")
+html = response.read()
+
+class IMDBHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        
+        self.startTag = []
+        self.attrs = []
+        self.data = []
+        self.endTag = []
+        self.current = []
+
+        self.inTable = False
+        self.inTBody = False
+        self.inTRow = False
+        self.inTcol = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'table':
+            self.startTag.append(tag)
+            self.attrs.append(attrs)
+            self.inTable = True
+        elif tag == 'tbody':
+            self.startTag.append(tag)
+            self.attrs.append(attrs)
+            self.inTBody = True;
+        elif tag == 'tr':
+            if len(attrs) > 0:
+                self.startTag.append(tag)
+                self.attrs.append(attrs)
+                self.inTRow = True
+        elif tag == 'td':
+            self.startTag.append(tag)
+            self.attrs.append(attrs)
+            self.inTcol = True
+
+    def handle_endtag(self, tag):
+        if tag == 'table':
+            self.endTag.append(tag)
+            self.inTable = False
+        elif tag == 'tbody':
+            self.endTag.append(tag)
+            self.inTBody = False
+        elif tag == 'tr':
+            self.endTag.append(tag)
+            if self.inTRow:
+                self.data.append(self.current)
+                self.current = []
+            self.inTRow = False
+        elif tag == 'td':
+            self.endTag.append(tag)
+            self.inTcol = False
+
+    def handle_data(self, data):
+        if self.inTcol and self.inTRow:
+            self.current.append(data)
+
+
+parser = IMDBHTMLParser()
+parsedData = parser.feed(html)
+
+print parser.data
+print parser.startTag
+print parser.endTag
+
