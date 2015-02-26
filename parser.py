4aa04c5944e47172cf88f17d0868f9e2d12e4982
Alexander Premer <Premer@outlook.com>
2015-02-26 15:16:16 +0100

    Parser now uses the findshow class to create url and looks a little cleaner and is commented

--- parser.py
+++ parser.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
 from HTMLParser import HTMLParser
-import urllib2 as url
-import json
-
-response = url.urlopen("http://www.imdb.com/title/tt1358522/tvschedule")
-html = response.read()
+from findShows import IMDBShowInfo as imdb
+import urllib2 as url, json
 
 class IMDBHTMLParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
-        
+        # Lists to store the info about the schedule
         self.data = []
         self.current = []
 
+        # If the parser is in a row/col of a table
+        # we updates the data sets
         self.inTRow = False
         self.inTcol = False
-
+    
+    # What to do with when startag is found
     def handle_starttag(self, tag, attrs):
         if tag == 'tr':
             if len(attrs) > 0:
@@ -24,23 +24,42 @@ class IMDBHTMLParser(HTMLParser):
         elif tag == 'td':
             self.inTcol = True
 
+    # What to do with when endtag is found
     def handle_endtag(self, tag):
         if tag == 'tr':
+            # We are now about to leave the row so
+            # we need to store the current data as a list
+            # in the dataset and set row to false
             if self.inTRow:
-                self.data.append(self.current)
-                self.current = []
+               self.data.append(self.current)
+               self.current = []
             self.inTRow = False
         elif tag == 'td':
             self.inTcol = False
 
+    # What to do with when inbetween start- and endtag
     def handle_data(self, data):
+        # If we are in a coloumn and a row the data is stored as
+        # current list
         if self.inTcol and self.inTRow:
             self.current.append(data)
 
+# Gets the show's info
+show = imdb("suits")
 
+# Creates the url for the tvschedule of the show
+baseurl = "http://www.imdb.com/title/{0}/tvschedule".format(show.getID())
+
+# Fetches the html of the url
+response = url.urlopen(baseurl)
+html = response.read()
+
+
+# Starts the parser and feeds the html
 parser = IMDBHTMLParser()
 parsedData = parser.feed(html)
 
+# Prints to test if it works
 for dataentry in parser.data:
     date = dataentry[0]
     time = dataentry[1]
@@ -49,4 +68,3 @@ for dataentry in parser.data:
     episode = dataentry[5]
 
     print "Date : {0}\nTime: {1}\nName: {2} {3}\n\n".format(date, time, name, episode)
-
