78c7851c24e126c1024cb38b90de0cc73e707c4c
Alexander Premer <Premer@outlook.com>
2015-02-26 12:25:18 +0100

    added a method whichs finds the shows id

--- findShows.py
+++ findShows.py
@@ -2,18 +2,23 @@
 
 import urllib, sys
 
-#baseurl
+# Baseurl
 baseurl = "http://www.omdbapi.com/?"
 
-#queries
+# Queries
 apiVars = { 't': sys.argv[1], 'r' : 'json'}
 
 # Query to the api requesting the show passed as arg.
 query = baseurl+urllib.urlencode(apiVars)
 
 # The Json returned by the api without curly brackets
-data = urllib.urlopen(query).read()[1:-1].split(',')
+info = urllib.urlopen(query).read()[1:-1].split(',')
 
-# Just printing all the data about the show
-for d in data:
-   print d
+# Returns the ID of the show
+def getID(showInfo):
+   for field in showInfo:
+      if 'imdbID' in field:
+         return field
+
+
+print getID(info)
