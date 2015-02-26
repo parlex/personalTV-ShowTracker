512c8f0c21d6bc927b22b03dfd5ca6484543e875
Alexander Premer <Premer@outlook.com>
2015-02-26 14:57:01 +0100

    Restructured the code such that it is now a class where you can get different values from the info

--- findShows.py
+++ findShows.py
@@ -2,23 +2,63 @@
 
 import urllib, sys
 
-# Baseurl
-baseurl = "http://www.omdbapi.com/?"
+class IMDBShowInfo():
+   def __init__(self, showname):
+      # Baseurl
+      self.baseurl = "http://www.omdbapi.com/?"
+   
+      # Queries
+      self.apiVars = { 't': showname, 'r' : 'json'}
+   
+      # Query to the api requesting the show passed as arg.
+      self.query = self.baseurl+urllib.urlencode(self.apiVars)
+   
+      # The Json returned by the api without curly brackets
+      self.info = urllib.urlopen(self.query).read()[1:-1].split(',')
 
-# Queries
-apiVars = { 't': sys.argv[1], 'r' : 'json'}
+      self.showDict = self.transformToDict()
+ 
+   # Transforms the info list to a dictionary
+   def transformToDict(self):
+      output = {}
+      for entry in self.info:
+         if 'Title' in entry or 'Poster' in entry or 'imdbRating' in entry or 'imdbID' in entry:
+            output[entry.split(":")[0].strip("\"")] = entry.split(":")[1].strip("\"")
+               
+      return output
+   
+   # Returns the show title
+   def getTitle(self):
+      return self.showDict.get("Title")
+   
+   # Returns the show poster this doesn't work yet
+   def getPoster(self):
+      return self.showDict.get("Poster")
+   
+   # Returns the show  IMDB rating
+   def getRating(self):
+      return self.showDict.get("imdbRating")
+   
+   # Returns the show ID on IMDB
+   def getID(self):
+      return self.showDict.get("imdbID")
 
-# Query to the api requesting the show passed as arg.
-query = baseurl+urllib.urlencode(apiVars)
+###################
+# example usage
+###################
+#data = IMDBShowInfo("suits")
+#
+#print data.showDict
+#print data.getTitle()
+#print data.getPoster()
+#print data.getRating()
+#print data.getID()
 
-# The Json returned by the api without curly brackets
-info = urllib.urlopen(query).read()[1:-1].split(',')
 
-# Returns the ID of the show
-def getID(showInfo):
-   for field in showInfo:
-      if 'imdbID' in field:
-         return field
-
-
-print getID(info)
+# gives output : 
+# 
+# {'Poster': 'http', 'imdbID': 'tt1632701', 'imdbRating': '8.8', 'Title': 'Suits'}
+# Suits
+# http
+# 8.8
+# tt1632701
